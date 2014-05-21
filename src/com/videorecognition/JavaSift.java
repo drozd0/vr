afa21b507047c445d1ef737c80734ed3b87276f6
dubrovin <mihail.dubrovin@gmail.com>
2014-05-21 19:12:07 +0400

    add existed project

---
+++ src/com/videorecognition/JavaSift.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.videorecognition;
+
+/**
+ *
+ * @author Mike
+ */
+public class JavaSift {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Test2("c:\\tmp\\1.png","c:\\tmp\\2.jpg").test(null, null);
+    }
+}
